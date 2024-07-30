0d1cf2d2d419fbb539b1ef885e3edee576883898
Griff-123 <griffinstechmesser@gmail.com>
2024-07-29 22:46:56 -0500

    Final Revision

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = text_count(text)
     final_dict = (get_dictionary(text))
-    print(f"""-------- Report of {book_path}--------
-          """)
-    for i in final_dict:
-        for x in i:
-            print(f"The {x} character was found in the book {i[x]} times.")
-    print(f"""
-The total number of words in this book is {num_words}
-""")
-    print("----------------- End Of Report -----------------")
+    final_report(book_path, final_dict, num_words )
+
+def get_book_text(x):
+    with open(x) as f:
+        return f.read()
+
+def text_count(y):
+    words = y.split()
+    return len(words)
 
 def get_dictionary(z):
     default_dict = {}
@@ -22,23 +22,24 @@ def get_dictionary(z):
                 default_dict[i] += 1
             else:
                 default_dict[i] = 1
-    new_dict = sorted(default_dict.items(), key=lambda x:x[1], reverse=True)
-    set_list = dict(new_dict)
-    new_list = [{k: v} for k,v in set_list.items()]
-    
-    return new_list
+    return sort_dictionary(default_dict)
 
+def sort_dictionary(unsorted_dict):
+    list = sorted(unsorted_dict.items(), key=lambda x:x[1], reverse=True)
+    new_dict = dict(list)
+    final_dict = [{k: v} for k,v in new_dict.items()]
+    return final_dict
 
-def text_count(y):
-    words = y.split()
-    return len(words)
-
-def get_book_text(x):
-    with open(x) as f:
-        return f.read()
-
-    
-
+def final_report(a, b, c, ):
+    print(f"""-------- Report of {a}--------
+          """)
+    for i in b:
+        for x in i:
+            print(f"The {x} character was found in the book {i[x]} times.")
+    print(f"""
+The total number of words in this book is {c}
+""")
+    print("----------------- End Of Report -----------------")   
 
 
 main()
