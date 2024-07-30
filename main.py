ae8a53ee71602ec50a754a822e672e90e47cc3e5
Griff-123 <griffinstechmesser@gmail.com>
2024-07-29 22:28:45 -0500

    everything working, not pretty

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = text_count(text)
-    print(get_dictionary(text))
+    final_dict = (get_dictionary(text))
+    print(f"""-------- Report of {book_path}--------
+          """)
+    for i in final_dict:
+        for x in i:
+            print(f"The {x} character was found in the book {i[x]} times.")
+    print(f"""
+The total number of words in this book is {num_words}
+""")
+    print("----------------- End Of Report -----------------")
 
 def get_dictionary(z):
     default_dict = {}
     lower = z.lower()
     for i in lower:
-        if i in default_dict:
-            default_dict[i] += 1
-        else:
-            default_dict[i] = 1
-    return default_dict
+        if i.isalpha() == True:
+            if i in default_dict:
+                default_dict[i] += 1
+            else:
+                default_dict[i] = 1
+    new_dict = sorted(default_dict.items(), key=lambda x:x[1], reverse=True)
+    set_list = dict(new_dict)
+    new_list = [{k: v} for k,v in set_list.items()]
+    
+    return new_list
 
 
 def text_count(y):
@@ -22,6 +36,9 @@ def text_count(y):
 def get_book_text(x):
     with open(x) as f:
         return f.read()
+
+    
+
 
 
 main()
